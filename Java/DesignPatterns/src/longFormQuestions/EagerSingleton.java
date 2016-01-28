145a5cb12917bb2ca2beb19abe7d2a3ec0b7476e
BBK-PiJ-2014-26 <gmoore04@dcs.bbl.ac.uk>
2016-01-28 19:39:02 +0000

    Java folder added

---
+++ Java/DesignPatterns/src/longFormQuestions/EagerSingleton.java
@@ -0,0 +1,13 @@
+package longFormQuestions;
+
+public class EagerSingleton {
+
+	private static EagerSingleton instance = new EagerSingleton();
+	
+	private EagerSingleton() {
+	}
+	
+	public static EagerSingleton getInstance() {
+		return instance;
+	}
+}
---
+++ Java/DesignPatterns/src/longFormQuestions/SingletonProtected.java
@@ -0,0 +1,5 @@
+package longFormQuestions;
+
+public class SingletonProtected {
+
+}
