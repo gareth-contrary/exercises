93be2c11f451c8ee82199c73d730f0f66185d2cd
Gareth <garethmoore26@gmail.com>
2016-02-01 14:48:20 +0000

    Q3 answered

--- week4/src/part1/Expressions.scala
+++ week4/src/part1/Expressions.scala
@@ -8,21 +8,19 @@ object Expressions {
   def main(args : Array[String]) {
    
   def isSunny(weather : String, temperature : Int) : Boolean = {
-    if (weather.equals("sunny") && (temperature >= 80)) {
-      true
-    } else {
-      false
-    }
+    if (weather.equals("sunny") && (temperature > 80)) { true }
+    else { false }
   }
 
   def isSunnyOrCloudy(weather : String, temperature : Int) : Boolean = {
-    if (weather.equals("sunny") || weather.equals("partly cloudy") && (temperature >= 80) {
-      true
-    } else {
-      false
-    }
+    if ((temperature > 80) && (weather.equals("sunny") || weather.equals("partly cloudy"))) { true } 
+    else { false }
   }
  
-   println(isSunnyOrCloudy("sunny", 83))
+  def isSunnyCloudy80Or20(weather : String, temperature : Int) : Boolean = {
+    if ((temperature > 80 || temperature < 20) && (weather.equals("sunny") || weather.equals("partly cloudy"))) { true } 
+    else { false }
+  }
+   println(isSunnyCloudy80Or20("sunny", 15))
   }
 }
