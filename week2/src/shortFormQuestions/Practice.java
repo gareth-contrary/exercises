74a9180460a25389507c1aa2d82e23606447f3e1
Gareth <garethmoore26@gmail.com>
2016-02-01 12:24:16 +0000

    gg

---
+++ week2/src/shortFormQuestions/Practice.java
@@ -0,0 +1,10 @@
+package shortFormQuestions;
+
+public interface Practice {
+
+	double x = 2.3;
+	
+	Practice(double x);
+	Practice();
+	
+}
---
+++ week2/src/shortFormQuestions/PracticeImpl.java
@@ -0,0 +1,11 @@
+package shortFormQuestions;
+
+public class PracticeImpl implements Practice {
+
+	public static void main(String[] args) {
+		Practice p = new PracticeImpl();
+		System.out.println(p.x);
+
+	}
+
+} 
