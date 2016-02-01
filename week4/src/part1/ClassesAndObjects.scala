2be317c6224a6a360e7e6f4ee51023c436f05188
Gareth <garethmoore26@gmail.com>
2016-02-01 18:39:36 +0000

    Fields in classes done

--- week4/src/part1/ClassesAndObjects.scala
+++ week4/src/part1/ClassesAndObjects.scala
@@ -53,7 +53,46 @@ object ClassesAndObjects {
     assert(a1 == "Motor on", "Expected Motor on, Got " + a1)
     val a2 = motorboat.off()
     assert(a2 == "Motor off", "Expected Motor off, Got " + a2)
+  
+    //Methods inside classes q2
+    val flare = new Flare
+    val f1 = flare.light
+    assert(f1 == "Flare used!", "Expected Flare used!, Got " + f1)
+    
+
+    
+    //Methods inside classes q3
+    val sailboat2 = new Sailboat
+    val signal = sailboat2.signal()
+    assert(signal == "Flare used!", "Expected Flare used!, Got " + signal)
+    val motorboat2 = new Motorboat
+    val flare2 = motorboat2.signal()
+    assert(flare2 == "Flare used!", "Expected Flare used!, Got " + flare2)
+  
+    
+    import com.atomicscala.AtomicTest._
+    
+    //Fields in classes
+    //val cup = new Cup
+    //cup.add(45) is 45
+    //cup.add(-15) is 30
+    //cup.add(-50) is -20
+    
+    val cup = new Cup
+    cup.add(45) is 45
+    cup.add(-55) is 0
+    cup.add(10) is 10
+    cup.add(-9) is 1
+    cup.add(-2) is 0
+
+    //cup.percentFull = 56
+    //cup.percentFull is 56  
+    
+    cup.set(56)
+    cup.get() is 56
   }
+  
+  
   
   class Hippo{}
   class Lion{}
@@ -64,10 +103,44 @@ object ClassesAndObjects {
   class Sailboat{
     def lower() = {println("Sails lowered")}  
     def raise() = {println("Sails raised")}
+    def signal():String = {
+      val f = new Flare
+      f.light()
+    }
   }
   
   class Motorboat{
     def on():String = {"Motor on"}  
     def off():String = {"Motor off"}
+    def signal():String = {
+      val f = new Flare
+      f.light()
+    }
+  }
+  
+  class Flare{
+    def light():String = "Flare used!"
+  }
+  
+  class Cup {
+
+    private var percentFull = 0
+    val max = 100
+    
+    def add(increase:Int):Int = {
+      percentFull += increase
+      if(percentFull > max) {
+        percentFull = max
+     }
+      if (percentFull < 0) {
+        percentFull = 0
+      }
+      percentFull 
+    }
+    
+    def get() : Int = {percentFull}
+    def set(percentFull : Int) = {this.percentFull = percentFull}
   }
 }
+      
+
