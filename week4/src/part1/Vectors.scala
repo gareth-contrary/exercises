1f01afbdc81cda40ef2738952b54199de86ceca0
Gareth <garethmoore26@gmail.com>
2016-02-01 19:35:07 +0000

    almost there

--- week4/src/part1/Vectors.scala
+++ week4/src/part1/Vectors.scala
@@ -1,5 +1,23 @@
 package part1
 
-class Vectors {
+object Vectors {
   
+  def main(args : Array[String]) {
+  
+    val species =  Vector("sapiens", "neanderthalis", "floresiensis", "habilis", "erectus")
+    
+    var str : String = " belongs to the genus Homo."
+    
+    for(word <- species) 
+      println(word)
+      
+    import com.atomicscala.AtomicTest._
+    
+    val vec = Vector("The", "dog", "visited", "the", "fire", "station")
+    var sentence : String = ""
+    for(word <- vec)
+      sentence = sentence + word + " "
+
+    sentence.toString() is "The dog visited the fire station "
+  }
 }
