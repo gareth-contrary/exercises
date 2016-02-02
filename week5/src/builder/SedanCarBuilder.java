4839dcc265e7992e7676e81d09a395066c88c9de
Gareth <garethmoore26@gmail.com>
2016-02-02 23:05:52 +0000

    Q6 done

--- week5/src/builder/SedanCarBuilder.java
+++ week5/src/builder/SedanCarBuilder.java
@@ -10,37 +10,40 @@ public class SedanCarBuilder implements CarBuilder {
 	
 	@Override
 	public void buildBodyStyle() {
-		car.setBodyStyle("");
+		car.setBodyStyle("External dimensions: overall length (inches): 202.9,\n"
+				+ "  overall width (inches): 76.2, overall height (inches): 60.7,\n"
+				+ "  wheelbase (inches): 112.9, front track (inches): 65.3,\n"
+				+ "  rear track (inches): 65.5 and curb to curb turning circle (feet): 39.5");
 	}
 
 	@Override
 	public void buildPower() {
-		car.setPower("");
+		car.setPower("285 hp @ 6,500 rpm; 253 ft lb of torque @ 4,000 rpm");
 	}
 
 	@Override
 	public void buildEngine() {
-		car.setEngine("");
+		car.setEngine("3.5L Duramax V 6 DOHC");
 	}
 
 	@Override
 	public void buildBreaks() {
-		car.setBreaks("");
+		car.setBreaks("Four-wheel disc brakes: two ventilated. Electronic brake distribution");
 	}
 
 	@Override
 	public void buildSeats() {
-		car.setSeats("");
+		car.setSeats("Front seat centre armrest.Rear seat centre armrest.Split-folding rear seats");
 	}
 
 	@Override
 	public void buildWindows() {
-		car.setWindows("");
+		car.setWindows("Laminated side windows.Fixed rear window with defroster");
 	}
 
 	@Override
 	public void buildFuelType() {
-		car.setFuelType("");
+		car.setFuelType("Diesel 19 MPG city, 29 MPG highway, 23 MPG combined and 437 mi. range");
 	}
 
 	@Override
--- week5/src/builder/SportsCarBuilder.java
+++ week5/src/builder/SportsCarBuilder.java
@@ -10,37 +10,42 @@ public class SportsCarBuilder implements CarBuilder {
 	
 	@Override
 	public void buildBodyStyle() {
-		car.setBodyStyle("");
+		car.setBodyStyle("External dimensions: overall length (inches): 192.3,\n"
+				+ "  overall width (inches): 75.5, overall height (inches): 54.2,\n"
+				+ "  wheelbase (inches): 112.3, front track (inches): 63.7,\n"
+				+ "  rear track (inches): 64.1 and curb to curb turning circle (feet): 37.7");
 	}
 
 	@Override
 	public void buildPower() {
-		car.setPower("");
+		car.setPower("323 hp @ 6,800 rpm; 278 ft lb of torque @ 4,800 rpm");
 	}
 
 	@Override
 	public void buildEngine() {
-		car.setEngine("");
+		car.setEngine("3.6L V 6 DOHC and variable valve timing");
 	}
 
 	@Override
 	public void buildBreaks() {
-		car.setBreaks("");
+		car.setBreaks("Four-wheel disc brakes: two ventilated. Electronic brake distribution.\n"
+				+ "  Stability control");
 	}
 
 	@Override
 	public void buildSeats() {
-		car.setSeats("");
+		car.setSeats("Driver sports front seat with one power adjustments manual height,\n"
+				+ "  front passenger seat sports front seat with one power adjustments");
 	}
 
 	@Override
 	public void buildWindows() {
-		car.setWindows("");
+		car.setWindows("Front windows with one-touch on two windows");
 	}
 
 	@Override
 	public void buildFuelType() {
-		car.setFuelType("");
+		car.setFuelType("Petrol 17 MPG city, 28 MPG highway, 20 MPG combined and 380 mi. range");
 	}
 
 	@Override
