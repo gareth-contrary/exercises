65b0286fbc4264826145d64e3f955f19434a110b
Gareth <garethmoore26@gmail.com>
2016-02-06 11:47:34 +0000

    First commit

---
+++ week5/src/composite/HtmlElement.java
@@ -0,0 +1,29 @@
+package composite;
+
+public class HtmlElement extends HtmlTag {
+
+	@Override
+	public String getTagName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartTag(String tag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setEndTag(String tag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void generateHtml() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ week5/src/composite/HtmlParentElement.java
@@ -0,0 +1,29 @@
+package composite;
+
+public class HtmlParentElement extends HtmlTag {
+
+	@Override
+	public String getTagName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartTag(String tag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setEndTag(String tag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void generateHtml() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
