23bda144427895eb81b4093b779d6875155dd046
Gareth <garethmoore26@gmail.com>
2016-02-11 23:41:39 +0000

    Q19 done

--- week6/src/paramterisedtypes/Explicit.scala
+++ week6/src/paramterisedtypes/Explicit.scala
@@ -22,9 +22,19 @@ object Explicit extends App {
     List.concat(vec)
   }
   
+  def explicitSet(vec: Vector[Double]): Set[Double] = {
+    var result: Set[Double] = Set()
+    for (x <- vec) {
+      result = result.+(x)
+    }
+    return result
+  }
+  
   inferred('a', 'b', 'c') is "Vector(a, b, c)"
   explicit('a', 'b', 'c') is "Vector(a, b, c)"
   explicitDouble(1.0, 2.0, 3.0) is Vector(1.0, 2.0, 3.0)
   explicitList(Vector(10.0, 20.0)) is List(10.0, 20.0)
   explicitList(Vector(1, 2, 3)) is List(1.0, 2.0, 3.0)
+  explicitSet(Vector(10.0, 20.0, 10.0)) is Set(10.0, 20.0)
+  explicitSet(Vector(1, 2, 3, 2, 3, 4)) is Set(1.0, 2.0, 3.0, 4.0)
 }
