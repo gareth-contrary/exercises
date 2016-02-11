ae76053bd2da83b5bcdf8786c23c1e9c770cd5b0
Gareth <garethmoore26@gmail.com>
2016-02-11 20:15:20 +0000

    Q7 done

--- week6/src/namedanddefaultargs/SimpleTime.scala
+++ week6/src/namedanddefaultargs/SimpleTime.scala
@@ -1,5 +1,5 @@
 package namedanddefaultargs
 
-case class SimpleTime(hours: Int, minutes: Int){
+case class SimpleTime(hours: Int, minutes: Int)
 
-}
+case class SimpleTime2(hours: Int, minutes: Int = 0)
--- week6/src/namedanddefaultargs/TestNamedAndDefaultArgs.scala
+++ week6/src/namedanddefaultargs/TestNamedAndDefaultArgs.scala
@@ -7,3 +7,8 @@ object TestNamedAndDefaultArgs extends App {
   val t = new SimpleTime(hours=5, minutes=30)
   t.hours is 5
   t.minutes is 30
+  
+  val t2 = new SimpleTime2(hours=10)
+  t2.hours is 10
+  t2.minutes is 0
+}
