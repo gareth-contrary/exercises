16bdfab01dd4f1dded312481937488838fb5a8ca
Gareth <garethmoore26@gmail.com>
2016-02-18 20:07:33 +0000

    Q1 done

--- week7/src/mapandreduce/Q1.scala
+++ week7/src/mapandreduce/Q1.scala
@@ -8,4 +8,22 @@ object Q1 extends App {
   val v = Vector(1, 2, 3, 4)
   v.map(n => n + ((n+1) * 10)) is Vector(21, 32, 43, 54)
   
+  
+  //part b
+  val v2 = Vector(1, 2, 3, 4)
+  v2.foreach(n => n + ((n+1) * 10)) is Vector(21, 32, 43, 54)  
+  //foreach will return a function
+  
+  //part c 
+  val v3 = Vector(1, 2, 3, 4)
+  def f(v: Vector[Int]): Vector[Int] =  {
+    var result = Vector[Int]()
+    for (x <- v.reverse) {
+      result = result.+:(x + (x+1) * 10)
+    }
+    result
+  }
+  
+  f(v3) is Vector(21, 32, 43, 54)  
+  
 }
