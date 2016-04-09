8e8f840d177d2daa0b0a7f7ea1b4046b8838ff13
Gareth <garethmoore26@gmail.com>
2016-04-09 23:23:41 +0100

    Adapter done

---
+++ week2/src/longFormQuestions/XPay.java
@@ -0,0 +1,28 @@
+package longFormQuestions;
+
+public interface XPay {
+	String getCreditCardNo();
+	
+	void setCreditCardNo(String creditCardNo);
+	
+	String getCustomerName();
+	
+	void setCustomerName(String customerName);	
+	
+	String getCardExpMonth();
+	
+	void setCardExpMonth(String cardExpMonth);
+	
+	String getCardExpYear();
+
+	void setCardExpYear(String cardExpYear);
+
+	Short getCardCVVNo();
+	
+	void setCardCVVNo(Short cardCVVNo);
+	
+	Double getAmount();
+	
+	void setAmount(Double amount);
+
+}
